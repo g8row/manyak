5a9cce80d7fd9fa03d60f0605db58b19769bb7da
gurow <aliogu23@gmail.com>
2021-06-22 21:39:06 +0300

    button

--- src/designs/BackButton.java
+++ src/designs/BackButton.java
@@ -1,13 +1,17 @@
 package designs;
 
 import javax.swing.*;
+import javax.swing.border.*;
+
 import java.awt.*;
 
 public class BackButton extends JPanel {
     public JButton backButton;
     //public static final int WIDTH =3
     public BackButton (){
-        backButton = new GButton();
+        backButton = new JButton();
+        //backButton.setBackground(Color.WHITE);
+        backButton.setBorder(new EmptyBorder(5,5,5,5));
         backButton.setIcon(new ImageIcon("src/designs/back-arrow.png"));
         setLayout(new BorderLayout());
         add(backButton,BorderLayout.WEST);
